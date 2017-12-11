d4b9de281ced5003f328a5e1d34b33c4d3670432
Benjamin <bhillenbrand101@gmail.com>
2017-12-10 22:42:51 -0600

    SQL interactions Done!

--- src/main/java/com/Benjamin/Load.java
+++ src/main/java/com/Benjamin/Load.java
@@ -3,10 +3,15 @@ package com.Benjamin;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 
-public class Load extends JFrame { // Much of this was learned from and used from Java2545 Examples
+public class Load extends JFrame { // Much of this was learned from and used from Java2545 Examples the ToDOList delete button and the two-windows
+                                        //  https://github.com/minneapolis-edu/Java2545Examples
     private JPanel loadFormPanel;
     private JList LoadList;
     private JButton loadButton;
@@ -33,8 +38,6 @@ public class Load extends JFrame { // Much of this was learned from and used fro
             listModel.addElement(nameForLoad.get(i));
         }
 
-
-
         LoadList.setModel(listModel);
 
         LoadList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -42,52 +45,71 @@ public class Load extends JFrame { // Much of this was learned from and used fro
 
         getRootPane().setDefaultButton(loadButton);
 
-
+ // If a person picks a game they want to load then
       loadButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                int selectedIndex = LoadList.getSelectedIndex();
-                if (selectedIndex != -1) {
-                    String nameLoadMePlease = listModel.get(selectedIndex);
+                int selectedIndex = LoadList.getSelectedIndex(); // Gets the index slot they selected.
+                if (selectedIndex != -1) { //if and item is selected will do the  following
+                    String nameLoadMePlease = listModel.get(selectedIndex); // Finding selected item on the list, making it a string
                     //  send data back to sql
-                    ArrayListOfSaveNames.setNameLoadMePleaseFromLoadScreen(nameLoadMePlease);
-                    parentComponent.setEnabled(true);
-                    parentComponent.recieveLoadReqest(playerHandArray, opponentLeftArray, teamMatesHandArray, checkDeck   );
-                    setVisible(false);
-                    Load.this.dispose();
-                } else {
-                    JOptionPane.showMessageDialog(LoadList , "Please select an item to delete");
+                    ArrayListOfSaveNames.setNameLoadMePleaseFromLoadScreen(nameLoadMePlease); // //sets name to a more easier to work with variable
+                    parentComponent.setEnabled(true); // Making the  GameLayout Gui active again
+                    parentComponent.recieveLoadReqest(playerHandArray, opponentLeftArray, teamMatesHandArray, checkDeck   ); // Sending back this information
+                    // Why you ask I did both ArrayListOfSaveNames.setNameLoadMePleaseFromLoadScreen() and the above? Basicly to challenge myself.
+                    setVisible(false); // Making the Load GUI Screen disappear.
+                    Load.this.dispose(); // Closing down the Load GUI screen.
+                } else { // In case something goes wrong
+                    JOptionPane.showMessageDialog(LoadList , "Please select an item to load");
                 }
 
             }
         });
 
+      // Delete button to remove unwanted saves from the list.
         deleteButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                Connection connection = null;
 
-                int selectedIndex = LoadList.getSelectedIndex();
-                if (selectedIndex != -1) {
-                    listModel.remove(selectedIndex);
-                    // TODO send data back to sql to delete.
+                int selectedIndex = LoadList.getSelectedIndex();  // Getting the index of the item selected on the list
+                if (selectedIndex != -1) { // If and item selected will hopefully do the following.
+                    String nameOfDeleteMe = listModel.get(selectedIndex); // Changing it to a string format to match it with the MySQL name slot.
+                    if (JOptionPane.showConfirmDialog(deleteButton, "Delete " + selectedIndex + "?", "Delete",
+                            JOptionPane.OK_CANCEL_OPTION) == JOptionPane.OK_OPTION) { // WIll show a comfiem option dialog button to make sure delete button was not accenditly pressed.
+                        listModel.remove(selectedIndex); // Removing the item from the shown list.
 
-                } else {
+                        try { // The purpose of this is to delete your choice from the shown list.
+                            connection = ConnectionConfiguration.getConnection(); // connecting to SQL
+                            PreparedStatement deleteSaveDataSQL = connection.prepareStatement( "DELETE FROM spades WHERE NAME = (?)");  // MySQL syntax for delete of selected
+                            deleteSaveDataSQL.setString(1, nameOfDeleteMe); // adding the name (string) of it to a premade statement
+                            deleteSaveDataSQL.executeUpdate(); // Deleting from SQL
+
+                            connection.close(); // Peace out, Yo.
+
+                        } catch (SQLException sqlE) { // Default action if nothing there so things don't crash.
+                            System.out.println("There was no " + nameOfDeleteMe + " to delete" + sqlE);
+                        }
+
+                    }
+
+                } else { // If button pressed an nothing selected.
                     JOptionPane.showMessageDialog(LoadList , "Please select an item to delete");
                 }
 
             }
         });
 
-        //TODO crash/freeze here after action?
+        // In case they change their mind
         cancelButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // System.exit(0);  // Nope closes everything down
                 //Load.this.EXIT_ON_CLOSE;
-                parentComponent.setEnabled(true);
-                setVisible(false);
-                Load.this.dispose();
+                parentComponent.setEnabled(true); // ReEnables the parent component.
+                setVisible(false); // Makes the load screen vanish
+                Load.this.dispose(); // Disvoles the  Load GUI
                 //return GameLayout;
             }
         });
